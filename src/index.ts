ddc0c8ae4c80eff0d47279ba109fdbc759b3180f
Sanket Singh <singhsanket143@gmail.com>
2024-01-15 22:31:35 +0530

    configured prisma

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import express , { Express, Request, Response } from 'express';
 
 import ServerConfig from './config/server.config';
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 const app : Express = express();
 
 app.get('/ping', (req: Request, res: Response) => {
     res.json({msg: 'ok'});
 })
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Server started at PORT: ${ServerConfig.PORT}`)
+app.listen(ServerConfig.PORT, async () => {
+    console.log(`Server started at PORT: ${ServerConfig.PORT}`);
+    const user = await prisma.user.findMany();
+    console.log(user);
+    console.log("End");
 })
